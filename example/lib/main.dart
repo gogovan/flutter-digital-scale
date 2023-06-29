42316cea48373efc2672b66a21d445e1ebfa1aa7
Peter WONG <peter.wong@gogotech.hk>
2023-06-29 14:36:04 +0800

    feat: EPT-2441: create example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_digital_scale/wxl_t12.dart';
+import 'package:flutter_digital_scale/weight.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,13 +29,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final scale = WXLT12();
+
+  var status = 'Finding printer...';
+  Stream<Weight>? stream;
+  Future<Weight>? stabilizedWeight;
+
+  @override
+  void initState() {
+    _connectPrinter();
+  }
+
+  void _connectPrinter() {
+    scale.connect((device, service) {
+      stream = scale.getWeightStream();
+      setState(() {
+        status = 'Printer connected';
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Digital Scale Demo"),
       ),
-      body: Column(),
+      body: Column(children: [
+        Text(status),
+        Text('Current Weight'),
+        StreamBuilder<Weight>(
+          stream: stream,
+          builder: (context, snapshot) => Text(
+            '${snapshot.data?.value} ${snapshot.data?.unit}',
+          ),
+        ),
+        ElevatedButton(
+            onPressed: getStabilizedWeight, child: Text('Stabilized Weight')),
+        FutureBuilder(
+          future: stabilizedWeight,
+          builder: (context, snapshot) => Text(
+            '${snapshot.data?.value} ${snapshot.data?.unit}',
+          ),
+        ),
+      ]),
     );
+  }
+
+  void getStabilizedWeight() {}
+
+  @override
+  void dispose() {
+    scale.disconnect();
   }
 }
